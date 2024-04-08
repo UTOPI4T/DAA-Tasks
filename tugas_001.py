6dbe944913e3a438d29f17dd8285b653460a5a5f
Nathan Rahmavanya <143484750+UTOPI4T@users.noreply.github.com>
2024-04-08 20:49:36 +0700

    tugas_001.py

--- tugas_001.py
+++ tugas_001.py
@@ -1,10 +1,12 @@
+"""
 1. Algoritma :
 a) Mencari KPK dari bilangan 3 dan 4.
 b) Nyatakan bilangan yang lebih besar diantara bilangan 3 dan 4.
 c) Bilangan yang lebih besar dihitung kelipatannya.
 d) Hasil kelipatan dibagi dengan bilangan yag satunya.
 e) Jika hasil bagi tidak sisa, cetak.
-f) Ulangi hingga kelipatan dapat dibagi oleh bilangan.
+f) Ulangi hingga kelipatan dapat dibagi oleh bilangan. 
+"""
 
 Pseudocode ;
 x= 3
@@ -22,11 +24,13 @@ while kelipatan % y != 0
        if kelipatan % y = 0
                   write(x)
 
+"""
 2) Algoritma ;
 a) Manggis di Piring 1, pisang di piring 2, dan piring 3 kosong.
 b) Manggis dipindahkan ke piring 3.
 c) Pisang dipindahkan ke piring 1.
 d) Manggis dipindahkan ke piring 2 dan piring 3 kosong.
+"""
 
 Pseudocode ;
 var  piring1, piring2, piring3:
@@ -39,12 +43,13 @@ piring1 = piring2
 piring2 = piring3
 
 
-
+"""
 3) Algoritma ;
 a) Menyatakan luas, alas, dan tinggi.
 b) Masukan alas = 25 dan tinggi = 30.
 c) Menghitung luas dengan rumus ½*alas*tinggi.
 d) Menampilkan hasil perhitungan luas.
+"""
 
 Pseudocode  ;
 var alas, tinggi , luas:
@@ -53,11 +58,14 @@ tinggi = 30
 luas= 1/2*alas*tinggi
 write(luas)
 
+"""
 4) Algoritma ;
 a) Menyatakan luas, panjang, dan tinggi.
 b) Masukkan panjang = 5 dan tinggi 3.
 c)  Menghitung luas dengan rumus panjang*tinggi.
 d) Menampilkan hasil perhitungan luas.
+"""
+
 Pseudocode ;
 var panjang, tinggi, luas:
 panjang = 5
@@ -65,11 +73,14 @@ tinggi = 3
 luas  = panjang*tinggi
 wirte(luas)
 
+"""
 5)Algoritma ;
 a) Menyatakan volume, panjang, dan tinggi.
 b) Masukkan jari-jari= 3 dan tinggi 5.
 c)  Menghitung volume dengan rumus π*jari-jari*jari-jari*tinggi
 d) Menampilkan hasil perhitungan volume.
+"""
+
 Pseudocode ;
 var jari-jari, tinggi, volume:
 jari-jari = 3
@@ -77,12 +88,15 @@ tinggi = 5
 volume = 3,14*jari-jari*jari-jari*tinggi
 write(volume)
 
+"""
 6.Algoritma ;
 a) Menyatakan volume, diameter, dan tinggi.
 b) Masukkan diameter = 5 dan tinggi 4.
 c) Mengkonversi diameter menjadi jari-jari dengan membagi 2 besar diameter.
 d)  Menghitung volume dengan rumus 1/3*π*jari-jari*jari-jari*tinggi
 e) Menampilkan hasil perhitungan volume.
+"""
+
 Pseudocode ;
 var diameter, tinggi, volume:
 diameter = 5
